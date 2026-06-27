071457500c7c327a386d22a6f08ca592fbe3e645
Thinh ND <thinhkiethanh123456@gmail.com>
2026-06-27 19:13:02 +0700

    realy add timeout, wal

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///database/app.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False},
+    connect_args={
+        "check_same_thread": False,
+        "timeout": 30
+    },
     pool_pre_ping=True
 )
 
@@ -14,3 +17,11 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+@event.listens_for(engine, "connect")
+def sqlite_pragmas(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL;")
+    cursor.execute("PRAGMA synchronous=NORMAL;")
+    cursor.execute("PRAGMA foreign_keys=ON;")
+    cursor.close()
