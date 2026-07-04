70a0139f7fa9fc8c61100ec63e176956945281d0
Thinh ND <thinhkiethanh123456@gmail.com>
2026-07-04 19:27:55 +0700

    refactor: chuyển đổi OtherRevenueRepository sang PostgreSQL

--- repositories/other_revenue_repository.py
+++ repositories/other_revenue_repository.py
@@ -1,107 +1,45 @@
 import pandas as pd
-
-from sqlalchemy import text
-
-from database.connection import engine
-
+# Import 2 hàm tiện ích từ file pg_database của bạn
+from database.pg_database import query_pg_to_dataframe, execute_pg_query
 from utils.datetime_utils import convert_utc_columns
-
 
 class OtherRevenueRepository:
 
     @staticmethod
     def get_all_revenues():
-
         query = """
-
         SELECT *
-
         FROM external_expenses
-
         ORDER BY expense_date DESC
-
         """
-
-        df = pd.read_sql(
-
-            query,
-
-            engine
-        )
-        
+        df = query_pg_to_dataframe(query)
         return convert_utc_columns(df)
 
     @staticmethod
     def add_revenue(
-
         revenue_date,
-
         amount,
-
         note,
-        
         created_by="System"
     ):
-
-        with engine.begin() as conn:
-
-            conn.execute(text("""
-
-            INSERT INTO external_expenses (
-
-                expense_date,
-
-                amount,
-
-                note,
-                
-                created_by
-
-            )
-
-            VALUES (
-
-                :expense_date,
-
-                :amount,
-
-                :note,
-                
-                :created_by
-
-            )
-
-            """),
-
-            {
-
-                "expense_date": revenue_date,
-
-                "amount": amount,
-
-                "note": note,
-                
-                "created_by": created_by
-            })
+        query = """
+        INSERT INTO external_expenses (
+            expense_date,
+            amount,
+            note,
+            created_by
+        )
+        VALUES (%s, %s, %s, %s)
+        """
+        
+        params = (revenue_date, amount, note, created_by)
+        execute_pg_query(query, params)
 
     @staticmethod
-    def delete_revenue(
-        revenue_id
-    ):
-
-        with engine.begin() as conn:
-
-            conn.execute(
-
-                text("""
-
-                DELETE FROM external_expenses
-
-                WHERE id = :id
-
-                """),
-
-                {
-                    "id": revenue_id
-                }
-            )
+    def delete_revenue(revenue_id):
+        query = """
+        DELETE FROM external_expenses
+        WHERE id = %s
+        """
+        
+        execute_pg_query(query, (revenue_id,))
